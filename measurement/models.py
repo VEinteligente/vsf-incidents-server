51ffb34bd2838dab42005de73c4256f9d0c776ed
Nestor Bracho <nestorbracho2207@gmail.com>
2016-10-18 15:25:16 -0400

    modelo de DNS

--- measurement/models.py
+++ measurement/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class DNS(models.Model):
+
+    isp = models.CharField(verbose_name='Operadora', max_length=50)
+    verbose = models.CharField(max_length=50)
+    ip = models.GenericIPAddressField()
+    public = models.BooleanField(default=True)
