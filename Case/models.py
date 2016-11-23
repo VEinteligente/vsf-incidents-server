02821e6e05c20512fd86e8169603242064054dde
promero <promero@turpialdev.com>
2016-11-23 16:32:48 -0400

    Case model and his migration

--- Case/models.py
+++ Case/models.py
@@ -1,5 +1,39 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from event.models import Event
 
-# Create your models here.
+
+class Case(models.Model):
+
+    TYPE_CATEGORIES = (
+        ('bloqueo', 'Bloqueo'),
+        ('desconexion', 'Desconexion'),
+        ('relentizacion', 'Relentizacion de servicio en Linea'),
+        ('conexion', 'Conexion inusualmente lenta'),
+        ('interceptacion', 'Interceptacion de trafico'),
+        ('falla', 'Falla Importante'),
+        ('dos', 'DoS')
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(null=True, blank=True)
+    category = models.CharField(choices=TYPE_CATEGORIES, max_length=20)
+    draft = models.BooleanField(default=True)
+    events = models.ManyToManyField(Event, related_name="cases")
+
+
+class Update(models.Model):
+    TYPE = (
+        ('info', 'Info'),
+        ('grave', 'Grave'),
+        ('positivo', 'Positivo')
+    )
+
+    date = models.DateTimeField()
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    category = models.CharField(choices=TYPE, max_length=20)
+    case = models.ForeignKey(Case)
