65692e4882f1ac1a63559b74971d5ccb7eb3547e
Nestor Bracho <nestorbracho2207@gmail.com>
2016-11-24 15:12:46 -0400

    tipo de evento

--- event/models.py
+++ event/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
@@ -6,10 +7,21 @@ from django.db import models
 
 class Event(models.Model):
 
+    TYPES = (
+        ('bloqueo por DPI', 'bloqueo por DPI'),
+        ('bloqueo por DNS', 'bloqueo por DNS'),
+        ('bloqueo por IP', 'bloqueo por IP'),
+        ('Interceptacion de trafico', 'Interceptación de tráfico'),
+        ('falla de dns', 'falla de dns'),
+        ('Velocidad de internet', 'Velocidad de internet'),
+        ('alteracion de trafico por intermediarios', 'alteración de tráfico por intermediarios')
+    )
+
     isp = models.CharField(max_length=25)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField(null=True, blank=True)
     target = models.CharField(max_length=25)  # input in metrics
     identification = models.CharField(max_length=50)
     draft = models.BooleanField(default=True)
+    type = models.CharField(max_length=100, choices=TYPES)
     # case = models.ForeignKey(Case)
