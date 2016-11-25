e6031835f98ab0f962c058887e7989296b0329cf
Andreina Marcano <milandre91@gmail.com>
2016-11-25 14:46:11 -0400

    rest api modifications TG-169, TG-171

--- Case/rest/urls.py
+++ Case/rest/urls.py
@@ -3,12 +3,12 @@ from Case.rest import views
 
 urlpatterns = [
     url(
-        r'^detail_case_rest/(?P<pk>[0-9]+)/$',
+        r'^detail_case_rest/(?P<case_id>[0-9]+)/$',
         views.DetailCaseRestView.as_view(),
         name='detail-case-rest'
     ),
     url(
-        r'^detail_event_case_rest/(?P<pk>[0-9]+)/$',
+        r'^detail_event_case_rest/(?P<case_id>[0-9]+)/$',
         views.DetailEventCaseRestView.as_view(),
         name='detail-event-case-rest'
     ),
--- Case/rest/views.py
+++ Case/rest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, status, exceptions
-from rest_framework.permissions import AllowAny
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from serializers import (
@@ -13,33 +14,21 @@ from django.http import Http404
 
 # Create your views here.
 
-class DetailCaseRestView(APIView):
+class DetailCaseRestView(generics.RetrieveAPIView):
 
+    #authentication_classes = (TokenAuthentication, BasicAuthentication)
+    #permission_classes = (IsAuthenticated,)
     permission_classes = (AllowAny,)
-
-    def get_object(self, pk):
-        try:
-            return Case.objects.get(pk=pk)
-        except Case.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        case = self.get_object(pk)
-        serializer = CaseSerializer(case)
-        return Response(serializer.data)
+    queryset = Case.objects.filter(draft=False)
+    lookup_url_kwarg = 'case_id'
+    serializer_class = CaseSerializer
 
 
-class DetailEventCaseRestView(APIView):
+class DetailEventCaseRestView(generics.RetrieveAPIView):
 
+    #authentication_classes = (TokenAuthentication, BasicAuthentication)
+    #permission_classes = (IsAuthenticated,)
     permission_classes = (AllowAny,)
-
-    def get_object(self, pk):
-        try:
-            return Case.objects.get(pk=pk)
-        except Case.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        case = self.get_object(pk)
-        serializer = DetailEventCaseSerializer(case)
-        return Response(serializer.data)
+    queryset = Case.objects.filter(draft=False)
+    lookup_url_kwarg = 'case_id'
+    serializer_class = DetailEventCaseSerializer
