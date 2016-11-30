28d2820bbdf67b8dd0c2cdf70817e1a6aeed3e0d
promero <promero@turpialdev.com>
2016-11-30 16:17:14 -0400

    new file utils.py with partial function suggestedEvents

--- event/front/utils.py
+++ event/front/utils.py
@@ -3,15 +3,31 @@ from measurement.models import Flag
 
 
 def suggestedEvents(flag):
-    """suggestedEvents: Function than assign a given hard flag (parameter)
+    """
+    suggestedEvents: Function than assign a given hard flag (parameter)
     to every existing event with same target and isp. This funstion save
     directly to DB and return true when the function finish successfully
     or false when a error happens during the execution of this function
     Args:
-        flag: Flag object"""
+        flag: Flag object with a hard flag data
+    """
     try:
+        # searching for events open ended with same target, same isp,
+        # and with associated flags with que same region
 
-        events = Event.objects.filter(isp=flag.isp, target=flag.target)
+        # this filter will return a queryset with duplicate events
+        events = Event.objects.filter(
+            isp=flag.isp,
+            target=flag.target,
+            end_date=None,
+            flags__probe__region=flag.probe.region)
+
+        # eliminate duplicate events in queryset
+        # defining a set with the queryset
+        events = set(events)
+
+        # assign every event in the list as a suggested_event of
+        # the hard flag
         for event in events:
             flag.suggested_events.add(event)
         return True
