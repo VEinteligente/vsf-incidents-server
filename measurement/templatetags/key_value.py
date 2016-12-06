bb8ddcb4f2a8aec43c5e39ef9a02a724faff5da0
Nestor Bracho <nestorbracho2207@gmail.com>
2016-12-06 15:43:39 -0400

    eztables

--- measurement/templatetags/key_value.py
+++ measurement/templatetags/key_value.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name='key_value')
 def key_value(dic, key):
     return dic[key]
--- vsf/settings.py
+++ vsf/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangojs',
+    'eztables',
     'rest_framework',
     'rest_framework.authtoken',
     'bootstrap3',
