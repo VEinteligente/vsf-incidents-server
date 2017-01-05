eb8cf709575fdc43c799f40ffdec6845fb419953
promero <promero@turpialdev.com>
2017-01-05 10:12:39 -0400

    solving merge

--- measurement/rest/views.py
+++ measurement/rest/views.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
-from measurement.models import Metric
-=======
-from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework import generics
-
-from measurement.rest import serializers
 from measurement.models import Metric, Flag
 from measurement.rest.serializers import FlagSerializer
->>>>>>> rest247
+
 
 from serializers import (
     MeasurementSerializer,
@@ -26,33 +17,18 @@ class MeasurementRestView(generics.ListAPIView):
     #   authentication_classes = (TokenAuthentication, BasicAuthentication)
     #   permission_classes = (IsAuthenticated,)
     permission_classes = (AllowAny,)
-<<<<<<< HEAD
+
     queryset = Metric.objects.all()
     serializer_class = MeasurementSerializer
-=======
-
-    def get(self, request):
-        """List measurements"""
-        serializer = serializers.MeasurementSerializer(
-            Metric.objects.all().first())
-        return Response(serializer.data)
->>>>>>> rest247
 
 
 class DNSMeasurementRestView(generics.ListAPIView):
     """DNSMeasurementRestView: MeasurementRestView
     for displaying a list of DNS measurements"""
-<<<<<<< HEAD
+
     permission_classes = (AllowAny,)
     queryset = Metric.objects.filter(test_name='dns_consistency')
     serializer_class = DNSMeasurementSerializer
-=======
-
-    def get(self, request):
-        """List DNS measurements"""
-        serializer = serializers.DNSMeasurementSerializer(
-            Metric.objects.filter(test_name='dns_consistency').first())
-        return Response(serializer.data)
 
 
 class FlagListView(generics.ListAPIView):
@@ -80,4 +56,4 @@ class MutedFlagListView(FlagListView):
     """MutedFlagListView: ListAPIView extends of FlagListView
     for displaying a list of all muted flags"""
     queryset = Flag.objects.filter(flag=None)
->>>>>>> rest247
+
