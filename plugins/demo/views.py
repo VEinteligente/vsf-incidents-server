99e57e9188a1eb0b6113f8ecac896878df35181f
promero <promero@turpialdev.com>
2017-02-09 16:41:18 -0400

    fix merge

--- plugins/demo/views.py
+++ plugins/demo/views.py
@@ -35,13 +35,3 @@ class DemoAjaxView(DatatablesView):
             json.dumps(data, cls=DjangoJSONEncoder)
         )
 
-
-def hola_hola():
-    print 'esto es una prueba exitosa'
-    return True
-
-
-def chao_chao():
-    print "WOOOOOOOOOOOOOOHOOOOOOOOOOOOOOO------------"
-    return False
-
--- plugins/urls.py
+++ plugins/urls.py
@@ -1,12 +1,6 @@
 from django.conf.urls import include, url
 from demo import urls as demo_url
-import views
 
 urlpatterns = [
     url(r'^demo/', include(demo_url, namespace='demo')),
-    url(
-        r'^test/$',
-        views.test.as_view(),
-        name='test'
-    ),
 ]
