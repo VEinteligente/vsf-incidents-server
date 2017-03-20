8a2f28c09206b3860b72aaef314e6fc31f0bf97a
promero <promero@turpialdev.com>
2017-03-20 15:02:55 -0400

    DNS and Http flag logics

--- plugins/dns/flag_logic.py
+++ plugins/dns/flag_logic.py
@@ -1,33 +1,124 @@
 from django.db.models.expressions import RawSQL
+from django.core.paginator import Paginator
+from django.conf import settings
+from django.utils.dateparse import parse_datetime
+from django.utils.timezone import make_aware
 
-from measurement.models import Metric
+from measurement.models import Metric, Flag
 from plugins.dns.models import DNS
 
 
 def web_connectivity_to_dns():
-    pass
+    SYNCRONIZE_DATE = settings.SYNCRONIZE_DATE
+    if SYNCRONIZE_DATE is not None:
+        SYNCRONIZE_DATE = make_aware(parse_datetime(settings.SYNCRONIZE_DATE))
+
+        web_connectivity_metrics = Metric.objects.filter(
+            test_name='web_connectivity',
+            measurement_start_time__gte=SYNCRONIZE_DATE
+        ).annotate(
+            queries=RawSQL(
+                "test_keys->'queries'", ()
+            ),
+            control_resolver=RawSQL(
+                "test_keys->'control'->'dns'", ()
+            )
+        ).values(
+            'id',
+            'measurement',
+            'queries',
+            'control_resolver'
+        )
+    else:
+        web_connectivity_metrics = Metric.objects.filter(
+            test_name='web_connectivity'
+        ).annotate(
+            queries=RawSQL(
+                "test_keys->'queries'", ()
+            ),
+            control_resolver=RawSQL(
+                "test_keys->'control'", ()
+            )
+        ).values(
+            'id',
+            'measurement',
+            'queries',
+            'control_resolver'
+        )
+
+    dns_paginator = Paginator(web_connectivity_metrics, 1000)
+
+    for p in dns_paginator.page_range:
+        page = dns_paginator.page(p)
+        for dns_metric in page.object_list:
+            cr = {}
+            try:
+                cr['failure'] = dns_metric['control_resolver']['dns']['failure']
+            except Exception:
+                cr['failure'] = None
+
+            try: 
+                cr['answers'] = {'addrs': dns_metric['control_resolver']['dns']['addrs']}
+            except Exception:
+                cr['answers'] = None
+
+            for query in dns_metric['queries']:
+                for answer in query['answers']:
+                    dns_exist = DNS.objects.filter(
+                        metric_id=dns_metric['id'],
+                        answers=answer,
+                    ).exists()
+                    if not dns_exist:
+                        dns = DNS(
+                            metric_id=dns_metric['id'],
+                            control_resolver_failure=cr['failure'],
+                            control_resolver_answers=cr['answers'],
+                            failure=query['failure'],
+                            answers=answer
+                        )
+                        dns.save()
 
 
 def dns_consistency_to_dns():
     # Get all metrics with test_name dns_consistency
     # but only values measurement, test_keys->'queries'
     # and test_keys->'control_resolver'
+    SYNCRONIZE_DATE = settings.SYNCRONIZE_DATE
+    if SYNCRONIZE_DATE is not None:
+        SYNCRONIZE_DATE = make_aware(parse_datetime(settings.SYNCRONIZE_DATE))
 
-    dns_consistency_metrics = Metric.objects.filter(
-        test_name='dns_consistency'
-    ).annotate(
-        queries=RawSQL(
-            "test_keys->'queries'", ()
-        ),
-        control_resolver=RawSQL(
-            "test_keys->'control_resolver'", ()
+        dns_consistency_metrics = Metric.objects.filter(
+            test_name='dns_consistency',
+            measurement_start_time__gte=SYNCRONIZE_DATE
+        ).annotate(
+            queries=RawSQL(
+                "test_keys->'queries'", ()
+            ),
+            control_resolver=RawSQL(
+                "test_keys->'control_resolver'", ()
+            )
+        ).values(
+            'id',
+            'measurement',
+            'queries',
+            'control_resolver'
         )
-    ).values(
-        'id',
-        'measurement',
-        'queries',
-        'control_resolver'
-    )
+    else:
+        dns_consistency_metrics = Metric.objects.filter(
+            test_name='dns_consistency'
+        ).annotate(
+            queries=RawSQL(
+                "test_keys->'queries'", ()
+            ),
+            control_resolver=RawSQL(
+                "test_keys->'control_resolver'", ()
+            )
+        ).values(
+            'id',
+            'measurement',
+            'queries',
+            'control_resolver'
+        )
 
     # for each dns metric get control resolver and other fields
 
@@ -64,6 +155,70 @@ def dns_consistency_to_dns():
                     dns.save()
 
 
+def dns_to_flag():
+    SYNCRONIZE_DATE = settings.SYNCRONIZE_DATE
+    if SYNCRONIZE_DATE is not None:
+        SYNCRONIZE_DATE = make_aware(parse_datetime(settings.SYNCRONIZE_DATE))
+        dnss = DNS.objects.filter(
+            metric__measurement_start_time__gte=SYNCRONIZE_DATE
+        )
+    else:
+        dnss = DNS.objects.all()
+
+    dnss = dnss.select_related('metric', 'flag')
+
+    dns_paginator = Paginator(dnss, 1000)
+    print dns_paginator.page_range
+
+    for p in dns_paginator.page_range:
+        page = dns_paginator.page(p)
+
+        for dns in page.object_list:
+            is_flag = False
+            if dns.metric.test_name == 'dns_consistency':
+                if dns.control_resolver_failure is None:
+                    if dns.failure == "no_answer":
+                        is_flag = True
+                    else:
+                        if dns.failure is None:
+                            if dns.control_resolver_answers != dns.answers:
+                                is_flag = True
+
+            if dns.metric.test_name == 'web_connectivity':
+                if (dns.control_resolver_failure is None) and (
+                    dns.control_resolver_answers is not None
+                ):
+                    if dns.failure == "no_answer":
+                        is_flag = True
+                    else:
+                        if dns.failure is None and dns.answers is not None:
+                            try:
+                                addr = dns.answers['ipv4']
+                            except Exception:
+                                try:
+                                    addr = dns.answers['ipv6']
+                                except Exception:
+                                    addr = dns.answers['hostname']
+                            if addr not in dns.control_resolver_answers['addrs']:
+                                is_flag = True
+
+            if dns.flag is not None:
+                if dns.flag.is_flag != is_flag:
+                    dns.flag.is_flag = is_flag
+                    dns.flag.save()
+            else:
+                print "Debi entrar aqui"
+                flag = Flag(
+                    is_flag=is_flag,
+                    metric_date=dns.metric.measurement_start_time
+                )
+                flag.save()
+                dns.flag = flag
+                dns.save()
+
+        print page
+
+
 def metric_to_dns():
     print "Start Web_connectivity test"
     web_connectivity_to_dns()
@@ -71,5 +226,8 @@ def metric_to_dns():
     print "Start dns_consistency test"
     dns_consistency_to_dns()
     print "End dns_consistency test"
+    print "Start Evaluate DNS Flags"
+    dns_to_flag()
+    print "End Evaluate DNS Flags"
 
 
--- plugins/http/flag_logic.py
+++ plugins/http/flag_logic.py
@@ -1,8 +1,9 @@
 from django.db.models.expressions import RawSQL
 from django.conf import settings
+from django.core.paginator import Paginator
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import make_aware
-from measurement.models import Metric
+from measurement.models import Metric, Flag
 from plugins.http.models import HTTP
 
 
@@ -25,12 +26,16 @@ def web_connectivity_to_http():
             ),
             body_length_match=RawSQL(
                 "test_keys->'body_length_match'", ()
+            ),
+            body_proportion=RawSQL(
+                "test_keys->'body_proportion'", ()
             )
         ).values(
             'id',
             'status_code_match',
             'headers_match',
-            'body_length_match'
+            'body_length_match',
+            'body_proportion'
         )
     else:
         web_connectivity_metrics = Metric.objects.filter(
@@ -44,18 +49,23 @@ def web_connectivity_to_http():
             ),
             body_length_match=RawSQL(
                 "test_keys->'body_length_match'", ()
+            ),
+            body_proportion=RawSQL(
+                "test_keys->'body_proportion'", ()
             )
         ).values(
             'id',
             'status_code_match',
             'headers_match',
-            'body_length_match'
+            'body_length_match',
+            'body_proportion'
         )
 
     for http_metric in web_connectivity_metrics:
         if (http_metric['status_code_match'] is not None) and (
             http_metric['body_length_match'] is not None) and (
-            http_metric['headers_match'] is not None
+            http_metric['headers_match'] is not None) and (
+            http_metric['body_proportion'] is not None
         ):
             http_exist = HTTP.objects.filter(
                 metric_id=http_metric['id']
@@ -65,8 +75,51 @@ def web_connectivity_to_http():
                     metric_id=http_metric['id'],
                     status_code_match=http_metric['status_code_match'],
                     headers_match=http_metric['headers_match'],
-                    body_length_match=http_metric['body_length_match']
+                    body_length_match=http_metric['body_length_match'],
+                    body_proportion=http_metric['body_proportion']
                 )
+                http.save()
+
+
+def http_to_flag():
+    SYNCRONIZE_DATE = settings.SYNCRONIZE_DATE
+    if SYNCRONIZE_DATE is not None:
+        SYNCRONIZE_DATE = make_aware(parse_datetime(settings.SYNCRONIZE_DATE))
+        https = HTTP.objects.filter(
+            metric__measurement_start_time__gte=SYNCRONIZE_DATE
+        ).select_related('metric')
+    else:
+        https = HTTP.objects.all().select_related('metric')
+
+    https = https.select_related('metric', 'flag')
+
+    http_paginator = Paginator(https, 1000)
+
+    for p in http_paginator.page_range:
+        page = http_paginator.page(p)
+
+        for http in page.object_list:
+            is_flag = False
+            if (not http.headers_match) and (
+                not http.body_length_match) or (
+                not http.status_code_match
+            ):
+                is_flag = True
+
+            if http.body_proportion <= settings.BODY_PROPORTION_LIMIT:
+                is_flag = True
+
+            if http.flag is not None:
+                if http.flag.is_flag != is_flag:
+                    http.flag.is_flag = is_flag
+                    http.flag.save()
+            else:
+                flag = Flag(
+                    is_flag=is_flag,
+                    metric_date=http.metric.measurement_start_time
+                )
+                flag.save()
+                http.flag = flag
                 http.save()
 
 
@@ -74,3 +127,6 @@ def metric_to_http():
     print "Start Web_connectivity test"
     web_connectivity_to_http()
     print "End Web_connectivity test"
+    print "Start HTTP to Flag"
+    http_to_flag()
+    print "End HTTP to Flag"
