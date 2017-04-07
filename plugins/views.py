0cec5de0db9a7e54a1f031392e6579f438d39966
promero <promero@turpialdev.com>
2017-04-07 12:11:26 -0400

    Event create functional. Enable to TCP flags. Migrations included

--- plugins/views.py
+++ plugins/views.py
@@ -9,9 +9,12 @@ from django.http import HttpResponse
 from django.core.serializers.json import DjangoJSONEncoder
 import json
 import time
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
 
 from event.front.forms import EventEvidenceForm
-from event.models import Event
+from event.models import Event, Url
+from measurement.models import Flag
 
 # from django.utils.module_loading import import_string
 # app_label = "demo"
@@ -57,6 +60,125 @@ class PluginTableView(
         if self.url_ajax is not None:
             context['url_ajax'] = self.url_ajax
         return context
+
+
+class PluginCreateEventView(
+    PluginTableView,
+    generic.CreateView
+):
+    model = Event
+    success_url = reverse_lazy('events:event_front:list-event')
+
+    def form_valid(self, form):
+        """
+        If the form is valid, save the associated model.
+        """
+        # Get flags values
+
+        # Object to save
+        self.object = form.save(commit=False)
+
+        # Case Event with flags
+        if form.cleaned_data['flags'] != "":
+            ###################################################
+            # Get Flags
+            ###################################################
+            flags = form.cleaned_data['flags'].split('&')
+
+            flags = filter(None, flags)
+            flags = Flag.objects.filter(
+                uuid__in=flags
+            ).select_related(
+                'metric__probe__isp'
+            )
+
+            ###################################################
+            # Get input and isp from flags.
+            # I have the warranty
+            # than these are the same for all
+            # this flags thanks to form validation
+            ###################################################
+            flags_input = flags.first().metric.input
+            try:
+                flags_isp = flags.first().dnss.resolver_hostname
+            except Exception:
+                try:
+                    flags_isp = flags.first().metric.probe.isp.name
+                except Exception:
+                    flags_isp = "Unknown"
+
+            target, created = Url.objects.get_or_create(
+                url=flags_input
+            )
+
+            self.object.target = target
+            self.object.isp = flags_isp
+
+            ###################################################
+            # Get Start and End date (Considering open ended case)
+            ###################################################
+            if not form.cleaned_data['open_ended']:
+                if form.cleaned_data['end_date'] is None:
+                    self.object.end_date = flags.latest(
+                        'metric_date').metric_date
+            else:
+                self.object.end_date = None
+
+            if form.cleaned_data['start_date'] is None:
+                self.object.start_date = flags.earliest(
+                    'metric_date').metric_date
+
+            ###################################################
+            # Save Event in database
+            ###################################################
+
+            self.object.save()
+
+            ###################################################
+            # Set Any no flag into soft manual flag
+            # Associate every flag to new event
+            # Clear every suggested event on each flag
+            ###################################################
+            for flag in flags:
+                if flag.flag == Flag.NONE and flag.manual_flag is False:
+                    flag.flag = Flag.SOFT
+                    flag.manual_flag = True
+                flag.event = self.object
+                flag.suggested_events.clear()
+                flag.save()
+
+            ###################################################
+            # Associated suggested hard flags to new event
+            ###################################################
+
+            # Here must call suggested flags method
+
+        # Case Event with no flags
+        else:
+            ###################################################
+            # Get Target from form fields
+            ###################################################
+            target, created = Url.objects.get_or_create(
+                site=form.cleaned_data['target_site'],
+                url=form.cleaned_data['target_url'],
+                ip=form.cleaned_data['target_ip']
+            )
+
+            self.object.target = target
+            ###################################################
+            # Set End date none in open ended case
+            ###################################################
+            if form.cleaned_data['open_ended']:
+                self.object.end_date = None
+
+            ###################################################
+            # Save Event in database
+            ###################################################
+
+            self.object.save()
+
+        print 'entre al view'
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class DatatablesView(EditableDatatablesView):
