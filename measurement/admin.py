cf97d32ad69d81d4b42dfc5b876954e771255d91
promero <promero@turpialdev.com>
2017-04-11 16:02:49 -0400

    Change field url in MutedInput from url to char. Add MutedInput in admin

--- measurement/admin.py
+++ measurement/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from models import DNS, Flag, Probe, State, Country, Plan, ISP, Metric
+from models import (
+    DNS, Flag, Probe, State, Country, Plan, ISP, Metric, MutedInput
+)
 
 admin.site.register(DNS)
 admin.site.register(Flag)
@@ -9,3 +11,4 @@ admin.site.register(Country)
 admin.site.register(Plan)
 admin.site.register(ISP)
 admin.site.register(Metric)
+admin.site.register(MutedInput)
