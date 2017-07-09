4f4ade246ec8a66df355a778ea829940c87b5388
promero <promero@turpialdev.com>
2017-07-09 18:11:26 -0400

    se agregaron buscadores y filtros a los modelos de metric y flag. Se agrego campo input a las referencias de metric y flag a las pruebas dns

--- measurement/admin.py
+++ measurement/admin.py
@@ -3,11 +3,22 @@ from models import (
     DNS, Flag, Probe, State, Country, Plan, ISP, Metric,
 )
 
+
+class MetricAdmin(admin.ModelAdmin):
+    list_filter = ('test_name', 'probe__identification')
+    search_fields = ('measurement', 'input')
+
+
+class FlagAdmin(admin.ModelAdmin):
+    list_filter = ('flag', 'manual_flag')
+    search_fields = ('metric__measurement', 'metric__input')
+
+
 admin.site.register(DNS)
-admin.site.register(Flag)
+admin.site.register(Flag, FlagAdmin)
 admin.site.register(Probe)
 admin.site.register(State)
 admin.site.register(Country)
 admin.site.register(Plan)
 admin.site.register(ISP)
-admin.site.register(Metric)
+admin.site.register(Metric, MetricAdmin)
--- plugins/admin.py
+++ plugins/admin.py
@@ -4,8 +4,13 @@ from dns.models import DNS
 from tcp.models import TCP
 from ndt.models import NDT, DailyTest
 
+
+class DNSAdmin(admin.ModelAdmin):
+    raw_id_fields = ("metric", "flag")
+
+
 admin.site.register(HTTP)
-admin.site.register(DNS)
+admin.site.register(DNS, DNSAdmin)
 admin.site.register(TCP)
 admin.site.register(NDT)
 admin.site.register(DailyTest)
