7d2cf7b8c25b842c5620a8b9dd4fc18f38d49d1d
Andres A <andresazp@gmail.com>
2017-07-10 21:12:30 -0400

    Cast as mearument id as str to compare with collisions

--- measurement/utils.py
+++ measurement/utils.py
@@ -74,15 +74,15 @@ def copy_from_measurements_to_metrics():
         page_copied=[]
         for measurement in page.object_list:
             i += 1
-            if measurement.id in collisions:
+            if str(measurement.id) in collisions:
                 # We don't want to update the metrics that already exists in
                 # the database.
 #                 collisions.remove(measurement.id)
-                td_logger.info('Colition found and averted for measurement %s - index on page %n' % (measurement.id, i) )                
+                td_logger.info('! Colition found and averted for measurement %s - index on page %n' % (measurement.id, i) )                
 #                 continue
 #                 td_logger.debug('not in DB')
             elif measurement.id in page_copied:
-                td_logger.error('Duplicated measurement ID in page - page: %n, iteration: %n - ID: %s' % (p, i, measurement.id)) 
+                td_logger.error('!!! Duplicated measurement ID in page - page: %n, iteration: %n - ID: %s' % (p, i, measurement.id)) 
 #                 continue
 #                 td_logger.debug('not already copied in page')
             else:
